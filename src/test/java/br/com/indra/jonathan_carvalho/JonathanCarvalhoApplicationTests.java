e96600d65f1ee6f75b54d8b07bd72849d6c4d1d4
JonathanCarv <fortecomoumcarvalho@gmail.com>
2026-03-23 23:28:47 -0300

    Projeto

---
+++ src/test/java/br/com/indra/jonathan_carvalho/JonathanCarvalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.indra.jonathan_carvalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonathanCarvalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
